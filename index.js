6c1a0c74595ccd3c743878a16134aa0d05e4a86e
Fireblade75 <Fireblade75@users.noreply.github.com>
2017-09-21 15:15:04 +0200

    QueryFilter and eslint

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+
 const app = express();
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname.join('/plubic')));
 
-app.listen(8080, function () {
-    console.log('Starting server on port 8080.')
-})
+app.listen(8080, function listen() {
+    console.log('Starting server on port 8080.');
+});
