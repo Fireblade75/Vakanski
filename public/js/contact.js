49ee274b2e79b69febed204d7dfb85730e8cb29e
Tim Scholten <timscholten97@gmail.com>
2017-09-28 17:30:48 +0200

    contact - text input

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,7 +1,20 @@
 _.onReady(function () {
     _('#contact-button').onClick(function () {
+
         _('.contact-intro-text').clear();
-        const input = _.create('input', { type='text'});
-        _('#contact-button').append(input);
+        const input = _.create('textarea', { id: 'input-contact' });
+        _('.contact-intro-text').append(input);
+
+
+        var button = document.getElementById('contact-button');
+        button.innerHTML = 'Verstuur';
+        button.id = 'contact-verstuur';
+
+        document.getElementById('contact-intro-header').innerHTML = 'Wat wilt u vragen?';
+
+
+    });
+    _('#contact-verstuur').onClick(function () {
+        
     });
 });
