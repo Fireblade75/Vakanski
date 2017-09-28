64cd0c91795782ff83071c8bb963a2f5983a9c05
Tim Scholten <timscholten97@gmail.com>
2017-09-28 19:31:30 +0200

    button contact

--- public/js/contact.js
+++ public/js/contact.js
@@ -1,20 +1,25 @@
 _.onReady(function () {
+    var active = false;
     _('#contact-button').onClick(function () {
-
-        _('.contact-intro-text').clear();
-        const input = _.create('textarea', { id: 'input-contact' });
-        _('.contact-intro-text').append(input);
-
-
-        var button = document.getElementById('contact-button');
-        button.innerHTML = 'Verstuur';
-        button.id = 'contact-verstuur';
-
-        document.getElementById('contact-intro-header').innerHTML = 'Wat wilt u vragen?';
-
-
-    });
-    _('#contact-verstuur').onClick(function () {
         
+        if (!active) {
+            active = true;
+            _('.contact-intro-text').clear();
+            const input = _.create('textarea', { id: 'input-contact' });
+            _('.contact-intro-text').append(input);
+
+            var button = document.getElementById('contact-button');
+            button.innerHTML = 'Verstuur';
+
+            document.getElementById('contact-intro-header').innerHTML = 'Wat wilt u vragen?';
+        } else {
+            
+            
+            document.getElementById('contact-intro-header').innerHTML = 'Helemaal klaar';
+
+            document.getElementById('contact-intro-text').innerHTML = 'Bedankt voor uw vraag, we nemen zo snel mogelijk contact met u op.'
+
+        }
+
     });
 });
